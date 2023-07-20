b6a264fa54824ea06643fb3aed6df1bbd7311432
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2023-07-20 13:19:16 -0400

    Added polls app & models migration maked

--- platziawardsapp/polls/models.py
+++ platziawardsapp/polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+'''
+    # Concepts:
+
+    - on_delete=models.CASCADE: When we delete a Question, the options will be deleted
+'''
+
+
+class Question(models.Model):
+    question_text = models.TextField
+    date = models.DateTimeField('Date published')
+
+
+class Choice(models.Model):
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.TextField
+    votes = models.BigIntegerField(default=0)
