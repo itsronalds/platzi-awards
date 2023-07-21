cb2e55a9ca954f2887496b850bba6fbc0eec0d60
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2023-07-21 15:51:24 -0400

    Added new urls

--- platziawardsapp/polls/views.py
+++ platziawardsapp/polls/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World')
+    return HttpResponse('You are in the home page')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You are accessing to detail of question number {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'Results of question number {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You are vote to question with the number {question_id}')
